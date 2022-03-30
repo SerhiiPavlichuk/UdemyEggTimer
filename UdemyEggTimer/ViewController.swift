9fde2a5a97a985a36f47cffff1b73204890f7caf
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2022-03-30 12:52:42 +0300

    second commit

--- UdemyEggTimer/ViewController.swift
+++ UdemyEggTimer/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime: Float = 0
+    var secondsPassed: Float = 0
+
+    @IBAction func eggHardnessButtonPressed(_ sender: UIButton) {
+
+        timer.invalidate()
+        progressBar.progress = 0.0
+
+        guard let hardness = sender.currentTitle else { return }
+        titleLabel.text = hardness
+        if let unwrapedSeconds = eggTimes[hardness] {
+            totalTime = Float(unwrapedSeconds)
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
     }
 
-
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let percentageProgress = secondsPassed / totalTime
+            progressBar.progress = percentageProgress
+            print(percentageProgress)
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
 }
+
 
